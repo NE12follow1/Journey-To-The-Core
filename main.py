442ec42fe47e305442fe8ebde3d7cff6bbaed2fb
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2023-12-05 15:01:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import pygame
-import random
-from settings import *
+import pygame               #Used for displaying the game
+import random               #Used for anything that requires randomness
+import tkinter as tk        #Used primarily for getting the width and height of the screen
+from settings import *      #A separate python file that holds the 
 
 #Initialises the pygame library
 pygame.init()
+
+screen = pygame.display.set_mode([width,height])
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
