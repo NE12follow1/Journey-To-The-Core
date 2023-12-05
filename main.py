322a3ef5c64e1b3383d2a363696dd4a65e7914fc
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2023-12-05 15:08:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame               #Used for displaying the game
 import random               #Used for anything that requires randomness
-import tkinter as tk        #Used primarily for getting the width and height of the screen
 from settings import *      #A separate python file that holds the 
 
 #Initialises the pygame library
@@ -13,3 +12,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    screen.fill((0,0,0))
