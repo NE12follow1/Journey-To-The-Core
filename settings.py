1657a0265bcf6bdbd0a4ff380343c54a6886d477
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2023-12-06 11:46:08 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,1 @@
-import tkinter as tk                #Used primarily for getting the width and height of the screen
-
-root = tk.Tk()
-
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
+fps = 60
