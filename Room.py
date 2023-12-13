f67274651a20c2e163ab8955f97e39f79bbc2ae3
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2023-12-13 12:25:12 +0000

    Update Room.py

--- Room.py
+++ Room.py
@@ -1,1 +1,27 @@
-
+class Room():
+    def __init__(self,roomID):
+        self.roomID = roomID                   #String
+        
+        if roomID[0]:                          #Boolean
+            self.northConnection = True 
+        else:
+            self.northConnection = False
+            
+        if roomID[1]:                          #Boolean
+            self.eastConnection = True
+        else:
+            self.eastConnection = False
+            
+        if roomID[2]:                          #Boolean
+            self.southConnection = True 
+        else:
+            self.southConnection = False
+            
+        if roomID[3]:                          #Boolean
+            self.westConnection = True 
+        else:
+            self.westConnection = False
+            
+        self.timesItemSearched = 0             #Integer from 0 - 3
+        self.enemyPresent = [False,""]         #List = [Boolean,Object]
+        self.boxPresent = [False,False]        #List = [Boolean,Boolean]
