dafbcbd71829f896fbd1ad263d116b3488639664
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2024-01-06 13:41:36 +0000

    Update Room.py

--- Room.py
+++ Room.py
@@ -2,22 +2,22 @@ class Room():
     def __init__(self,roomID):
         self.roomID = roomID                   #String
         
-        if roomID[0]:                          #Boolean
+        if roomID[0] == "1":                          #Boolean
             self.northConnection = True 
         else:
             self.northConnection = False
             
-        if roomID[1]:                          #Boolean
+        if roomID[1] == "1":                          #Boolean
             self.eastConnection = True
         else:
             self.eastConnection = False
             
-        if roomID[2]:                          #Boolean
+        if roomID[2] == "1":                          #Boolean
             self.southConnection = True 
         else:
             self.southConnection = False
             
-        if roomID[3]:                          #Boolean
+        if roomID[3] == "1":                          #Boolean
             self.westConnection = True 
         else:
             self.westConnection = False
