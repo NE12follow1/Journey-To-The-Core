e1410ad3f629a32a775a874da329b4af33e291ce
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2024-01-11 15:13:21 +0000

    Update TileMap.py

--- TileMap.py
+++ TileMap.py
@@ -1,12 +1,14 @@
 import pygame
 
 class TileMap(pygame.sprite.Sprite):
-    def __init__(self, roomSpriteSheet, tileMap):
+    def __init__(self, roomSpriteSheet, tileMap, scale):
         super().__init__()
         self.file = open(tileMap, mode = "r")
+        self.tilesSize = (1000 * scale, 1000 * scale)
         self.tiles = pygame.image.load(roomSpriteSheet)
+        self.tiles = pygame.transform.scale(self.tiles, self.tilesSize)
 
-    def loadTileMap(self):
+    def loadTileMap(self,scale):
         tileMap = []
         tileSize = 100
         for row in range(4):
@@ -18,51 +20,52 @@ class TileMap(pygame.sprite.Sprite):
             line = self.file.readline()
             for y in range(4):
                 if line[y] == "0":
-                    tileMap[x][y] = (0,0,100,100)
+                    tileMap[x][y] = (0 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "1":
-                    tileMap[x][y] = (100,0,100,100)
+                    tileMap[x][y] = (100 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "2":
-                    tileMap[x][y] = (200,0,100,100)
+                    tileMap[x][y] = (200 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "3":
-                    tileMap[x][y] = (300,0,100,100)
+                    tileMap[x][y] = (300 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "4":
-                    tileMap[x][y] = (400,0,100,100)
+                    tileMap[x][y] = (400 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "5":
-                    tileMap[x][y] = (500,0,100,100)
+                    tileMap[x][y] = (500 * scale,0 * scale,100 * scale,100 * scale)
                 if line[y] == "6":
-                    tileMap[x][y] = (0,100,100,100)
+                    tileMap[x][y] = (0 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "7":
-                    tileMap[x][y] = (100,100,100,100)
+                    tileMap[x][y] = (100 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "8":
-                    tileMap[x][y] = (200,100,100,100)
+                    tileMap[x][y] = (200 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "9":
-                    tileMap[x][y] = (300,100,100,100)
+                    tileMap[x][y] = (300 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "A":
-                    tileMap[x][y] = (400,100,100,100)
+                    tileMap[x][y] = (400 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "B":
-                    tileMap[x][y] = (500,100,100,100)
+                    tileMap[x][y] = (500 * scale,100 * scale,100 * scale,100 * scale)
                 if line[y] == "C":
-                    tileMap[x][y] = (0,200,100,100)
+                    tileMap[x][y] = (0 * scale,200 * scale,100 * scale,100 * scale)
                 if line[y] == "D":
-                    tileMap[x][y] = (100,200,100,100)
+                    tileMap[x][y] = (100 * scale,200 * scale,100 * scale,100 * scale)
                 if line[y] == "E":
-                    tileMap[x][y] = (200,200,100,100)
+                    tileMap[x][y] = (200 * scale,200 * scale,100 * scale,100 * scale)
                 if line[y] == "F":
-                    tileMap[x][y] = (300,200,100,100)
+                    tileMap[x][y] = (300 * scale,200 * scale,100 * scale,100 * scale)
                 if line[y] == "G":
-                    tileMap[x][y] = (400,200,100,100)
+                    tileMap[x][y] = (400 * scale,200 * scale,100 * scale,100 * scale)
                 if line[y] == "H":
-                    tileMap[x][y] = (0,300,100,100)
+                    tileMap[x][y] = (0 * scale,300 * scale,100 * scale,100 * scale)
                 if line[y] == "I":
-                    tileMap[x][y] = (100,300,100,100)
+                    tileMap[x][y] = (100 * scale,300 * scale,100 * scale,100 * scale)
                 if line[y] == "J":
-                    tileMap[x][y] = (200,300,100,100)
+                    tileMap[x][y] = (200 * scale,300 * scale,100 * scale,100 * scale)
                 if line[y] == "K":
-                    tileMap[x][y] = (300,300,100,100)
+                    tileMap[x][y] = (300 * scale,300 * scale,100 * scale,100 * scale)
 
         return tileMap
 
-    def draw(self, tileMap, screen):
+    def draw(self, tileMap, screen, scale):
         for x in range(4):
             for y in range(4):
-                screen.blit(self.tiles, (y * 100, x * 100), tileMap[x][y])
+                self.location = (y * 100 * scale, x * 100 * scale)
+                screen.blit(self.tiles, self.location, tileMap[x][y])
