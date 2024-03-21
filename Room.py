82e03a4f75c8ce21cfb42e4abb5448606a1c4fc6
Matthew Jones <123167148+NE12follow1@users.noreply.github.com>
2024-03-21 18:16:55 +0000

    Update Room.py

--- Room.py
+++ Room.py
@@ -1,5 +1,5 @@
 class Room():
-    def __init__(self,roomID):
+    def __init__(self,roomID,enemyPresent = False,enemyObject = "",boxPresent = False,isExitRoom = False):
         self.roomID = roomID                   #String determining the type of room it is
         
         if roomID[0] == "1":                   #Boolean determining if the room connects to another room above it
@@ -22,6 +22,7 @@ class Room():
         else:
             self.westConnection = False
             
-        self.itemSearched = False              #Boolean determining if there is still a place to search for an item in the room
-        self.enemyPresent = [False,""]         #List = [Boolean for the existence of an enemy in the room, Object for the enemy itself]
-        self.boxPresent = [False,False]        #List = [Boolean for the existence of an equipment box in the room, Boolean for whether the box is or isn't open]
+        self.itemSearched = False                      #Boolean determining if there is still a place to search for an item in the room
+        self.enemyPresent = [enemyPresent,enemyObject] #List = [Boolean for the existence of an enemy in the room, Object for the enemy itself]
+        self.boxPresent = [boxPresent,False]           #List = [Boolean for the existence of an equipment box in the room, Boolean for whether the box is or isn't open]
+        self.isExitRoom = isExitRoom                   #Boolean determining if this room is the exit room
